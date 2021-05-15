226724184992e3fe86c43bdf9947992aaec470d3
paramveersoni1 <paramveer.tr.mdb@gmail.com>
2021-05-15 14:39:03 +0530

    first commit

---
+++ assets/js/slick.js
@@ -0,0 +1,57 @@
+$('.intergration_imgs').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    responsive:[
+      {
+        breakpoint:991,
+        settings: {
+        slidesToShow:4,
+        }
+      },
+      {
+        breakpoint:768,
+        settings: {
+          slidesToShow:3,
+        }
+      },
+      {
+        breakpoint:768,
+        settings: {
+          slidesToShow:3,
+        }
+      },
+    ]
+  });
+
+
+//   header_slider
+$('.slide_img').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    responsive:[
+      {
+        breakpoint:991,
+        settings: {
+        slidesToShow:4,
+        }
+      },
+      {
+        breakpoint:768,
+        settings: {
+          slidesToShow:3,
+        }
+      },
+      {
+        breakpoint:768,
+        settings: {
+          slidesToShow:3,
+        }
+      },
+    ]
+  });
